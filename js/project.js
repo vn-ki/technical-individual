e8e9bcfaf611b79ca40e30b3916b7209062d5d9a
aneeshnema <aneeshnema@gmail.com>
2018-06-20 23:26:27 +0530

    Cretated basic layout
    created basic layou of projects page,
    added projects.js, projects.css

---
+++ js/project.js
@@ -0,0 +1,27 @@
+var app = new Vue({
+  el: '#app-project',
+  data: {
+    projects: [
+      {
+        name: 'Project Name',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        bgImage: 'img/Vue.js_Logo.png'
+      },
+      {
+        name: 'Project Name2',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        bgImage: 'img/Vue.js_Logo.png'
+      },
+      {
+        name: 'Project Name3',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        bgImage: 'img/Vue.js_Logo.png'
+      },
+      {
+        name: 'Project Name4',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        bgImage: 'img/Vue.js_Logo.png'
+      }
+    ]
+  }
+});
