dc9fecee2bac6ead5232fd08383da814f027d1ea
Mrigank Krishan <mrigankkrishan@gmail.com>
2018-08-01 23:53:14 +0530

    i was a good boy and used sass compiler

--- js/footer.js
+++ js/footer.js
@@ -1,26 +1,29 @@
 Vue.component("footer-component", {
-  template: `
+    template: `
   <div class="container-fluid bg-dark text-white py-1" id="footer">
     <div class="container">
       <h3 class="py-2">Connect with us</h3>
       <ul class="list-inline">
         <li class="list-inline-item pb-3">
-            <a target="_blank" href="https://github.com/pclubiiti">
+            <a class="rounded" target="_blank" href="https://github.com/pclubiiti">
                 <i class="fab fa-github"></i>
                 <p class="pl-2">view our work</p>
             </a>
         </li>
         <li class="list-inline-item pb-3">
-            <a target="_blank" href="https://www.facebook.com/pclubiiti/">
+            <a class="rounded" target="_blank" href="https://www.facebook.com/pclubiiti/">
                 <i class="fab fa-facebook"></i>
                 <p class="pl-2">follow us on facebook</p>
             </a>
         </li>
         <li class="list-inline-item pb-3">
-            <a target="_blank" href="mailto:progclubiiti@iiti.ac.in">
+            <a class="rounded" target="_blank" href="mailto:progclubiiti@iiti.ac.in">
                 <i class="far fa-envelope"></i>
                 <p class="pl-2">mail us</p>
             </a>
+        </li>
+        <li class="list-inline-item pb-3" style="float:right">
+            Maintained by <a href="http://github.com/DaemonLab/" target="_blank" > Daemon Labs </a>
         </li>
       </ul>
     </div>
@@ -28,6 +31,6 @@ Vue.component("footer-component", {
   `
 });
 
-new Vue ({
-  el: "#foot",
+new Vue({
+    el: "#foot",
 });
