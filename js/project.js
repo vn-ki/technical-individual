3434b08c9f838b902ef5f306dac812d92465bc27
Devansh Shah <shahdevansh2000@gmail.com>
2018-08-02 06:25:00 +0530

    fix by devansh(3)

--- js/project.js
+++ js/project.js
@@ -1,8 +1,9 @@
 Vue.component("project-component", {
   data: function() {
     return {
-      projects: [
-        {
+      projects:{
+"2017":[    
+ {
           image: 'https://pclubiiti.github.io/img/project/game1.png',
           imagealt: 'game1',
           modalhref: '#projectModal1',
@@ -79,12 +80,89 @@ Vue.component("project-component", {
           date: '21st july 2017',
           domain: 'Machine Learning',
         },
-      ]
+],
+"2018": [
+	{
+          image: 'https://pclubiiti.github.io/img/project/ml1.svg',
+          imagealt: 'ml1',
+          modalhref: '#projectModal7',
+          modalhreftitle: 'projectModal7',
+          title: 'Mess@IITI',
+          content: 'The Mobile Application helps students at IIT-I to buy and use mess e-coupons based on per meal scheme.The e-coupons can also be transferred from one student to another.',
+          github: 'https://github.com/Deluser-7/Mess',
+          date: '21st july 2017',
+          domain: 'Machine Learning',
+        },
+	{
+          image: 'https://pclubiiti.github.io/img/project/ml1.svg',
+          imagealt: 'ml1',
+          modalhref: '#projectModal7',
+          modalhreftitle: 'projectModal7',
+          title: 'Nightmare (video game)',
+          content: 'Nightmare is survival shooter video game developed using Unity3D engine and is written in C#. Its a simple arcade style shooter where you kill enemies and earn points and create High Score. ',
+          github: 'https://github.com/Ronak-B/IITISoC_Project/tree/master/Survival%20Shooter',
+          date: '21st july 2017',
+          domain: 'Machine Learning',
+        },
+	{
+          image: 'https://pclubiiti.github.io/img/project/ml1.svg',
+          imagealt: 'ml1',
+          modalhref: '#projectModal7',
+          modalhreftitle: 'projectModal7',
+          title: 'Technical Clubs Website',
+          content: 'We created the websites for Programming, Electronics and CAE club.',
+          github: 'https://github.com/DaemonLab/technical-individual',
+          date: '21st july 2017',
+          domain: 'Machine Learning',
+        },
+	{
+          image: 'https://pclubiiti.github.io/img/project/ml1.svg',
+          imagealt: 'ml1',
+          modalhref: '#projectModal7',
+          modalhreftitle: 'projectModal7',
+          title: 'IITI MESS MANAGEMENT',
+          content: 'It is a client-side web-development project which facilitates buying and selling of food-coupons online. ',
+          github: 'https://github.com/vandan-agarwal/IITI-MESS-MANAGEMENT',
+          date: '21st july 2017',
+          domain: 'Machine Learning',
+        },
+	{
+          image: 'https://pclubiiti.github.io/img/project/ml1.svg',
+          imagealt: 'ml1',
+          modalhref: '#projectModal7',
+          modalhreftitle: 'projectModal7',
+          title: 'The Literary club website',
+          content: 'The Literary Club website is made as a part of the IITISoC project under the web development section. ',
+          github: 'https://github.com/Ayyk-123/Ayyk- 123.github.io',
+          date: '21st july 2017',
+          domain: 'Machine Learning',
+        },
+      ]}
     };
   },
   template: `
+	<div>
+	<h1 class="display-4 text-white py-2">IITSoC 2018</h1>
     <div class="team row">
-      <div class="col-lg-4 d-flex align-items-stretch" v-for="project in projects" :key="project.id">
+
+	
+      <div class="col-lg-4 d-flex align-items-stretch" v-for="project in projects['2018']" :key="project.id">
+        <div class="card">
+          <div class="profile-container">
+            <img class="card-img-top" style="max-height:5em;width:unset;" :src="project.image" alt="">
+          </div>
+          <div class="card-body">
+            <h3 class="card-title">{{project.title}}</h3>
+            <p class="card-text mb-2">{{project.content}}</p>
+            <a v-bind:href="project.github"><i class="fab fa-github"></i> </a>
+          </div>
+        </div>
+      </div>	
+</div>
+
+<h1 class="display-4 text-white py-2">IITSoC 2017</h1>
+ <div class="team row">
+      <div class="col-lg-4 d-flex align-items-stretch" v-for="project in projects['2017']" :key="project.id">
         <div class="card">
           <div class="profile-container">
             <img class="card-img-top" style="max-height:5em;width:unset;" :src="project.image" alt="">
@@ -97,6 +175,7 @@ Vue.component("project-component", {
         </div>
       </div>
     </div>
+	</div>
   `
 });
 
